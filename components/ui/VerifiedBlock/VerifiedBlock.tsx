2fb1b19243d802558471ecf485d4ca0e3734437b
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-06-20 01:49:12 +0200

    add timestamp to signature message

--- components/ui/VerifiedBlock/VerifiedBlock.tsx
+++ components/ui/VerifiedBlock/VerifiedBlock.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useSignMessage } from "wagmi"
 import { ConnectButton } from "@rainbow-me/rainbowkit"
 import { verifyMessage } from "ethers/lib/utils"
@@ -12,11 +12,12 @@ type Props = {
 }
 
 const VerifiedBlock = ({ beforeConnect, beforeSign, children }: Props) => {
-  const message = `Sign this message to prove you have access to this wallet in order to sign in to thegraph.com/studio.
+  const [timestamp] = useState(Date.now())
+  const message = `Sign this message to prove you have access to this wallet in order to sign in to redeem.slice.so.
 
   This won't cost you any Ether.
   
-  Timestamp: Now`
+  Timestamp: ${timestamp}`
 
   const { account, isAccountVerified, setIsAccountVerified } = useAppContext()
   const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage({

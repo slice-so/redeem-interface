908087a3d6dfe42f4548a791a47253e7e889d5c2
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-03-06 19:15:40 +0100

    remove buyer address from SubmissionBlock

--- components/ui/SubmissionBlock/SubmissionBlock.tsx
+++ components/ui/SubmissionBlock/SubmissionBlock.tsx
@@ -1,26 +1,18 @@
 import Chevron from "@components/icons/Chevron"
 import { Submission } from "@prisma/client"
 import { useState } from "react"
-import { useEnsName } from "wagmi"
-import { QuestionValue } from "../CreateFormInput/CreateFormInput"
 
 type Props = {
   submission: Submission & { createdAt: string }
+  index: number
 }
 
-const SubmissionBlock = ({ submission }: Props) => {
+const SubmissionBlock = ({ submission, index }: Props) => {
   const [isOpen, setIsOpen] = useState(false)
 
-  const { buyer, redeemedUnits, createdAt, answers } = submission
+  const { redeemedUnits, createdAt, answers } = submission
   const date = new Date(createdAt).toLocaleDateString()
 
-  // @ts-ignore
-  const { data: ens } = useEnsName({ address: buyer })
-
-  const reducedAddress = buyer.replace(
-    buyer.substring(5, buyer.length - 3),
-    `\xa0\xa0\xa0\xa0\xa0\xa0`
-  )
   const orderedQuestions = Object.keys(answers).sort()
 
   return (
@@ -33,16 +25,7 @@ const SubmissionBlock = ({ submission }: Props) => {
       onClick={() => (orderedQuestions.length != 0 ? setIsOpen(!isOpen) : null)}
     >
       <div className="grid items-center justify-between grid-cols-5 px-4 py-2 text-sm">
-        <p className="col-span-2">
-          <a
-            className="highlight"
-            href={`https://etherscan.io/address/${buyer}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {ens || reducedAddress}
-          </a>
-        </p>
+        <p className="col-span-2 text-left">{index}</p>
         <p className="text-center">{redeemedUnits}</p>
         <p className="col-span-2 text-right">{date}</p>
       </div>
